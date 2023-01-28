52cfd45c2b8d167324d84f6028e45e3726235151
현준용 <hjy4649@cocen.com>
2023-01-28 15:14:20 +0900

    feat: 기간나누기 header ui, method 추가

--- CALENDAR/src/views/division.vue
+++ CALENDAR/src/views/division.vue
@@ -1,5 +1,14 @@
 <template>
   <div id="calendar">
+    <header>
+      <ul>
+        <li v-for="(item, index) in menuBtn" :key="index">
+          <button :class="{ on: item.on }" @click="menu(index)">
+            {{ item.name }}
+          </button>
+        </li>
+      </ul>
+    </header>
     <transition name="fade">
       <Calendar v-model="today" />
     </transition>
@@ -11,4 +20,20 @@ import Calendar from "../components/Calendar.vue";
 import { ref } from "vue";
 
 const today = new Date();
+let menuBtn = ref([
+  { name: "DAY", on: false },
+  { name: "WEEK", on: false },
+  { name: "MONTH", on: true },
+  { name: "YEAR", on: false },
+]);
+
+const menu = (i) => {
+  menuBtn.value.map((item) => {
+    item.on = false;
+  });
+
+  menuBtn.value[i].on = true;
+};
 </script>
+
+<style scoped></style>
